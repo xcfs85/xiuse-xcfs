96089afe4227c3ddc648cb949e448fa51fa7715b
xcfs85 <xcfs85@126.com>
2016-12-09 13:00:19 +0800

    Initial commit

---
+++ xiuse/xiuse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(xiuse.Startup))]
+namespace xiuse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
