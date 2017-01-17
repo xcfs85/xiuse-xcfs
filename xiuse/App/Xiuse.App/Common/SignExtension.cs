0c22fd0c20f3b38753894b11b40e51f0765c6f3c
xcfs85@126.com <xcfs85@126.com>
2017-01-17 13:53:19 +0800

    添加token验证

---
+++ xiuse/App/Xiuse.App/Common/SignExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Xiuse.App.Common
+{
+    public class SignExtension
+    {
+        /// <summary>
+        /// 验证签名是否有效
+        /// </summary>
+        /// <param name="timeStamp"></param>
+        /// <param name="nonce"></param>
+        /// <param name="staffId"></param>
+        /// <param name="token"></param>
+        /// <param name="signature"></param>
+        /// <returns></returns>
+        public static bool Validate(string timeStamp, string nonce,string staffId, string token,string signature)
+        {
+            var hash = System.Security.Cryptography.MD5.Create();
+            //拼接签名数据
+            var signStr = timeStamp + nonce + staffId + token;
+            //将字符串中字符按升序排序
+            var sortStr = string.Concat(signStr.OrderBy(c => c));
+            var bytes = Encoding.UTF8.GetBytes(sortStr);
+            //使用MD5加密
+            var md5Val = hash.ComputeHash(bytes);
+            //把二进制转化为大写的十六进制
+            StringBuilder result = new StringBuilder(md5Val.ToString());
+            //foreach (var c in md5Val)
+            //{
+            //    result.Append(c.ToString("X2"));
+            //}
+
+            return result.ToString().ToUpper() == signature;
+        }
+    }
+}
---
+++ xiuse/App/Xiuse.App/Controllers/Security/ServiceController.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using Xiuse.App.Common;
+using Xiuse.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using DotNet.Utilities;
+
+namespace Xiuse.App.Controllers.Security
+{
+    public class ServiceController : ApiController
+    {
+        [Route("api/Authenticated")]
+        public HttpResponseMessage PostAuthenticated(string UserNameDS,string PassWordDS)
+        {
+            string UserName = DESEncrypt.Decrypt(UserNameDS);
+            string PassWord = DESEncrypt.Decrypt(PassWordDS);
+            ResultMsg resultMsg;
+            string staffId = "";
+            if (true)//验证用户名密码
+            {
+               
+               
+              
+            }
+            else
+            {
+              
+            }
+            //判断参数是否合法
+            if (string.IsNullOrEmpty(staffId))
+            {
+                resultMsg = new ResultMsg();
+                resultMsg.StatusCode = (int)StatusCodeEnum.ParameterError;
+                resultMsg.Info = StatusCodeEnum.ParameterError.GetEnumText();
+                resultMsg.Data = "";
+                return HttpResponseExtension.toJson(JsonConvert.SerializeObject(resultMsg));
+            }
+
+            //插入缓存
+            Token token = (Token)HttpRuntime.Cache.Get(staffId);
+            if (HttpRuntime.Cache.Get(staffId) == null)
+            {
+                token = new Token();
+                token.StaffId = staffId;
+                token.SignToken = Guid.NewGuid();
+                token.ExpireTime = DateTime.Now.AddDays(1);
+                HttpRuntime.Cache.Insert(token.StaffId.ToString(), token, null, token.ExpireTime, TimeSpan.Zero);
+            }
+
+            //返回token信息
+            resultMsg = new ResultMsg();
+            resultMsg.StatusCode = (int)StatusCodeEnum.Success;
+            resultMsg.Info = "";
+            resultMsg.Data = token;
+
+            return HttpResponseExtension.toJson(JsonConvert.SerializeObject(resultMsg));
+            return HttpResponseExtension.toJson(JsonConvert.SerializeObject(resultMsg));
+        }
+        /*
+        /// <summary>
+        /// 根据用户名获取token
+        /// </summary>
+        /// <param name="staffId"></param>
+        /// <returns></returns>
+        [Route("api/GetToken")]
+        public HttpResponseMessage GetToken(string staffId)
+        {
+            ResultMsg resultMsg = null;
+
+            //判断参数是否合法
+            if (string.IsNullOrEmpty(staffId) || (!string.IsNullOrEmpty(staffId)))
+            {
+                resultMsg = new ResultMsg();
+                resultMsg.StatusCode = (int)StatusCodeEnum.ParameterError;
+                resultMsg.Info = StatusCodeEnum.ParameterError.GetEnumText();
+                resultMsg.Data = "";
+                return HttpResponseExtension.toJson(JsonConvert.SerializeObject(resultMsg));
+            }
+
+            //插入缓存
+            Token token =(Token)HttpRuntime.Cache.Get(staffId);
+            if (HttpRuntime.Cache.Get(staffId) == null)
+            {
+                token = new Token();
+                token.StaffId = staffId;
+                token.SignToken = Guid.NewGuid();
+                token.ExpireTime = DateTime.Now.AddDays(1);
+                HttpRuntime.Cache.Insert(token.StaffId.ToString(), token, null, token.ExpireTime, TimeSpan.Zero);
+            }
+
+            //返回token信息
+            resultMsg =new ResultMsg();
+            resultMsg.StatusCode = (int)StatusCodeEnum.Success;
+            resultMsg.Info = "";
+            resultMsg.Data = token;
+
+            return HttpResponseExtension.toJson(JsonConvert.SerializeObject(resultMsg));
+        }
+        */
+    }
+}
