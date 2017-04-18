d24c9a8f2e59f4f20c3c21930950b01d63d4fa62
xcfs85 <xcfs85@126com>
2017-04-18 17:30:57 +0800

    dicountView

---
+++ xiuse/Libraries/Xiuse.Model/ViewModel/XiuseDicountView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xiuse.Model.ViewModel
+{
+    public class XiuseDicountView:Xiuse.Model.xiuse_discount
+    {
+        /// <summary>
+        /// 菜品详细信息
+        /// </summary>
+        private List<Xiuse.Model.xiuse_menus> _MenusDetail = new List<xiuse_menus>();
+        public List<Xiuse.Model.xiuse_menus> MenusDetail
+        {
+            get { return _MenusDetail; }
+            set { _MenusDetail = value; }
+        }
+    }
+}
