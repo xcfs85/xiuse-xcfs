142bfaf2720a8c68a25d5ac4f41d2b78619b0f58
xcfs85 <xcfs85@126.com>
2017-04-28 09:26:42 +0800

    缺失

---
+++ xiuse/Libraries/Xiuse.BLL/SysLog.cs
@@ -0,0 +1,36 @@
+﻿///增加操作日志（数据库表）
+/// 增加相应的函数和查询的接口
+///LogId（日志Id）、UserId（用户ID）、日志内容、日志类型、日志时间
+///
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Xiuse.BLL
+{
+    class SysLog
+    {
+        private readonly Xiuse.DAL.SysLog dal = new Xiuse.DAL.SysLog();
+        public SysLog() { }
+        /// <summary>
+        /// 增加一条数据
+        /// </summary>
+        /// <param name="model">对象实体</param>
+        public bool Insert(Xiuse.Model.SysLog model)
+        {
+            return dal.Insert(model);
+        }
+        /// <summary>
+        /// 通过id查询日志信息
+        /// </summary>
+
+        public Xiuse.Model.SysLog GetData(string Id)
+        {
+            return dal.GetModel(Id);
+        }
+
+    }
+}
